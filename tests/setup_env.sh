e0e6a778525396597b9b6bbb149a7e44fb45985d
lyuts <lyuts@users.sourceforge.net>
2010-07-31 06:05:52 +0000

    * Modified setup script and sql directory structure so our tests support testing several   DBMS without source code changes
    git-svn-id: https://visiual-db.svn.sourceforge.net/svnroot/visiual-db/trunk@218 a9af4176-ed96-4626-b17c-dfd41ec4b82a

--- tests/setup_env.sh
+++ tests/setup_env.sh
@@ -2,58 +2,117 @@
 
 cd `dirname $0`
 
-DBUSER=music_user
-DBNAME=music_db
-LANG=plpgsql
+if [ "x$1" = "x" ]; then
+    echo "Usage: `basename $0` DB_TYPE";
+    echo "       DB_TYPE: [psql, mysql, oracle, sqlite]";
+    exit 1;
+fi
 
-# create user if it doesn't exist already
-psql template1 postgres -c "\du" | grep $DBUSER > /dev/null
-if [ $? != 0 ]; then
-    createuser -U postgres --createdb --no-superuser --no-createrole $DBUSER
+######################
+# PSQL specific part #
+######################
+psqlSetup()
+{
+    DBUSER=music_user
+    DBNAME=music_db
+    LANG=plpgsql
 
+    # create user if it doesn't exist already
+    psql template1 postgres -c "\du" | grep $DBUSER > /dev/null
     if [ $? != 0 ]; then
-	echo "Unable to create db user!";
+        createuser -U postgres --createdb --no-superuser --no-createrole $DBUSER
+
+        if [ $? != 0 ]; then
+            echo "Unable to create db user!";
+            exit 1;
+        fi
+    fi
+
+    # create db if it doesn't exist already
+    psql template1 postgres -c "\l" | grep $DBNAME > /dev/null
+    if [ $? != 0 ]; then
+        createdb --host=localhost --username=$DBUSER --template=template1 $DBNAME
+
+        if [ $? != 0 ]; then
+            echo "Unable to create db!";
+            exit 1;
+        fi
+    fi
+
+    # create lang if it doesn't exist already
+    createlang --host=localhost --username=$DBUSER --dbname=$DBNAME -l | grep $LANG > /dev/null
+    if [ $? != 0 ]; then
+        createlang --host=localhost --username=$DBUSER --dbname=$DBNAME $LANG
+
+        if [ $? != 0 ]; then
+            echo "Unable to create lang!";
+            exit 1;
+        fi
+    fi
+
+    SQLS="sql/psql/drop.sql \
+          sql/psql/schemas.sql \
+          sql/psql/tables.sql \
+          sql/psql/views.sql \
+          sql/psql/triggers.sql \
+          sql/psql/indices.sql \
+          sql/psql/procedures.sql \
+          sql/psql/data.sql"
+
+    for i in $SQLS; do
+        echo "Applying $i";
+        psql -f $i $DBNAME $DBUSER > /dev/null
+
+        # no way check the last cmd for failure, somehow psql returns 0 on error
+
+    done
+}
+
+#######################
+# MYSQL specific part #
+#######################
+mysqlSetup()
+{
+
+}
+
+########################
+# SQLITE specific part #
+########################
+sqliteSetp()
+{
+
+}
+
+########################
+# ORACLE specific part #
+########################
+oracleSetup()
+{
+
+}
+
+########
+# MAIN #
+########
+
+case $1 in
+    psql)
+        psqlSetup;
+        ;;
+    mysql)
+        mysqlSetup;
+        ;;
+    oracle)
+        oracleSetup;
+        ;;
+    sqlite)
+        sqliteSetup;
+        ;;
+    *)
+        echo "[ERROR] Wrong db type!"
         exit 1;
-    fi
-fi
-
-# create db if it doesn't exist already
-psql template1 postgres -c "\l" | grep $DBNAME > /dev/null
-if [ $? != 0 ]; then
-    createdb --host=localhost --username=$DBUSER --template=template1 $DBNAME
-
-    if [ $? != 0 ]; then
-	echo "Unable to create db!";
-	exit 1;
-    fi
-fi
-
-# create lang if it doesn't exist already
-createlang --host=localhost --username=$DBUSER --dbname=$DBNAME -l | grep $LANG > /dev/null
-if [ $? != 0 ]; then
-    createlang --host=localhost --username=$DBUSER --dbname=$DBNAME $LANG
-
-    if [ $? != 0 ]; then
-	echo "Unable to create lang!";
-	exit 1;
-    fi
-fi
-
-SQLS="sql/drop.sql \
-     sql/schemas.sql \
-     sql/tables.sql \
-     sql/views.sql \
-     sql/triggers.sql \
-     sql/indices.sql \
-     sql/procedures.sql \
-     sql/data.sql"
-
-for i in $SQLS; do
-    echo "Applying $i";
-    psql -f $i $DBNAME $DBUSER > /dev/null
-
-    # no way check the last cmd for failure, somehow psql returns 0 on error
-
-done
+    ;;
+esac
 
 exit 0;

e6cac461cadb0349e90bff9a34800447b0e39bef
lyuts <lyuts@users.sourceforge.net>
2010-10-22 13:39:16 +0000

    * Use svn export instead of svn checkout. In this case we need less cleaning. * Do not include plugins into snapshots.
    git-svn-id: https://visiual-db.svn.sourceforge.net/svnroot/visiual-db/trunk@255 a9af4176-ed96-4626-b17c-dfd41ec4b82a

--- scripts/make_snp.sh
+++ scripts/make_snp.sh
@@ -5,23 +5,18 @@ TMP_DIR=visual-db
 cd `dirname $0`
 
 echo "FETCHING SNAPSHOT FROM SVN..."
-svn co https://visiual-db.svn.sourceforge.net/svnroot/visiual-db/trunk $TMP_DIR
+svn export https://visiual-db.svn.sourceforge.net/svnroot/visiual-db/trunk $TMP_DIR
 
 if [ $? != 0 ]; then
     echo "[ERROR] Unable to svn co!"
     exit -1;
 fi
 
-RM_DIRS=".svn .obj .moc tests scripts html latex bin"
-#RM_FILES="*.o *.a *~ *.so Makefile *Doxyfile* *.swp cscope* tags *core svn-commit*"
+RM_DIRS="tests scripts html latex bin plugins"
 
 for d in $RM_DIRS; do
-    find $TMP_DIR/ -name $d | xargs rm -rf
+    find $TMP_DIR/ -name $d -type d | xargs rm -rf
 done
-
-#for f in $RM_FILES; do
-#    find $TMP_DIR/ -name '$f' | xargs rm
-#done
 
 tar cfvz ${TMP_DIR}_snapshot_`date "+%Y%m%d"`.tar.gz $TMP_DIR
 

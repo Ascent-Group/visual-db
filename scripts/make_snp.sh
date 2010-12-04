dc7b8af9f65913f5b2ad3d03052602212bc09b71
lyuts <lyuts@users.sourceforge.net>
2010-12-04 13:12:46 +0000

    * Updated docs. * Remove intermediate .odt from snapshots.
    git-svn-id: https://visiual-db.svn.sourceforge.net/svnroot/visiual-db/trunk@358 a9af4176-ed96-4626-b17c-dfd41ec4b82a

--- scripts/make_snp.sh
+++ scripts/make_snp.sh
@@ -22,6 +22,9 @@ done
 sed -e 's/CONFIG.*= debug/CONFIG \+= release/g' $TMP_DIR/src/src.pro > $TMP_DIR/src/src.pro.new
 mv $TMP_DIR/src/src.pro.new $TMP_DIR/src/src.pro
 
+# remove Manual.odt
+rm -rf $TMP_DIR/doc/Manual.odt
+
 tar cfvz ${TMP_DIR}_snapshot_`date "+%Y%m%d"`.tar.gz $TMP_DIR
 
 rm -fr $TMP_DIR
